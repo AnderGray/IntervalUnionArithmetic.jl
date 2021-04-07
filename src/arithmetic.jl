5a587ff810b611ba1baee5dd44ff39e2f7f836e4
Ander Gray <ander.gray@liverpool.ac.uk>
2021-04-07 15:01:47 +0100

    adds divide

--- src/arithmetic.jl
+++ src/arithmetic.jl
@@ -2,11 +2,13 @@
 #   IntervalUnion Arithmetic
 ###
 
-for op in (:+, :-, :*, :/, :min, :max, :^, :log, :<, :>)
-    @eval ($op)( x::IntervalU, y::IntervalU) = intervalU([$op(xv, yv) for xv in x.v, yv in y.v][:])
+# Special case for /
+for op in (:+, :-, :/, :*, :min, :max, :^, :log, :<, :>)
+
+    if op != :/; @eval ($op)( x::IntervalU, y::IntervalU) = intervalU([$op(xv, yv) for xv in x.v, yv in y.v][:]); end
 
     @eval ($op)( x::IntervalU, y::Interval) = intervalU(broadcast($op, x.v, y))
-    @eval ($op)( x::Interval, y::IntervalU) = intervalU(broadcast($op, y, x.v))
+    if op != :/; @eval ($op)( x::Interval, y::IntervalU) = intervalU(broadcast($op, x, y.v)); end
 
     @eval ($op)( x::IntervalU, n::Real) = intervalU(broadcast($op, x.v, n))
     @eval ($op)( n::Real, x::IntervalU) = intervalU(broadcast($op, n, x.v))
@@ -15,6 +17,18 @@ end
 
 for op in (:-, :sin, :cos, :tan, :exp, :log)
     @eval ($op)( x::IntervalU) = intervalU(broadcast($op, x.v))
+end
+
+
+# Does x/y for y\{0} if 0 ∈ y
+function /(x :: IntervalU, y :: IntervalU)
+    yNew = bisect(y, 0)
+    return intervalU(broadcast(/, x.v, yNew.v))
+end
+
+function /(x :: Interval, y :: IntervalU)
+    yNew = bisect(y, 0)
+    return intervalU(broadcast(/, x, yNew.v))
 end
 
 function sqrt( x:: IntervalU)
