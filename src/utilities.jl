929a8a80cb623da7018d23d58491d2b4ff036efa
Ander Gray <ander.gray@liverpool.ac.uk>
2021-04-09 02:52:38 +0100

    adds in and isequal functions

--- src/utilities.jl
+++ src/utilities.jl
@@ -8,6 +8,18 @@ right(x :: Interval) = x.hi
 left(x :: IntervalU) = left.(x.v)
 right(x :: IntervalU) = right.(x.v)
 
+isequal(x :: IntervalU, y ::IntervalU) = all(isequal.(sort(x.v),sort(y.v)))
+
+function in( x:: IntervalU, y :: Vector{IntervalU})
+    for i =1:length(y)
+        if x == y[i]
+            return true
+        end
+    end
+    return false
+end 
+
+
 function Base.show(io::IO, this::IntervalU)
     v = sort(this.v)
     print(io, join(v, " ∪ "));
